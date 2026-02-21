e61c1c03569233951ddc824041a8c30fe572199c
Mariana Niño Solano <mnino499@unab.edu.co>
2026-02-20 22:40:58 -0500

    feat: agrega datos personales

--- app/src/main/java/me/mariananino/tallergitgithubapp/MainActivity.kt
+++ app/src/main/java/me/mariananino/tallergitgithubapp/MainActivity.kt
@@ -7,14 +7,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +34,7 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun MyPresentacion() {
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -46,5 +52,39 @@ fun MyPresentacion() {
                 .clip(CircleShape),
             contentScale = ContentScale.Crop
         )
+
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "MARIANA NIÑO",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(5.dp))
+        Text(
+            text = "Desarrolladora Android, Ingeniera de Sistemas y de Mercados",
+            fontSize = 16.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+        Divider()
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Text(text = "EDAD", fontWeight = FontWeight.Bold, fontSize = 12.sp)
+            Text(text = "20 años", fontSize = 14.sp)
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Text(text = "CORREO", fontWeight = FontWeight.Bold, fontSize = 12.sp)
+            Text(text = "mnino499@unab.edu.co", fontSize = 14.sp)
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Text(text = "CIUDAD", fontWeight = FontWeight.Bold, fontSize = 12.sp)
+            Text(text = "Bucaramanga, Colombia", fontSize = 14.sp)
+        }
     }
 }
